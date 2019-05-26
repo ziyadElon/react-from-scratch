0dc389899e093665bb9ea07edc218fdd66c74e75
“ziyadarfan” <ziyad.arfan@treniq.com>
2019-05-26 09:55:19 +0530

    final code

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+let Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+ReactDOM.render(<Home />, document.getElementById('app'));
